eb58d3a7681d530ff40cda4bf38744882742bf8a
Mahmuda <24524203082@student.bup.edu.bd>
2026-03-05 16:32:03 +0600

    Create 282A - Bit++.cpp

---
+++ 282A - Bit++.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n;  cin >> n;
+    string s;
+    int X = 0;
+    while(n--)
+    {
+        cin >> s;
+        if(s == "X++" || s == "++X")
+        X++;
+        else
+        X--;
+    }
+    cout << X << endl;
+    return 0;
+}
