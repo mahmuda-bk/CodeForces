075b04a1c787001ac0321579243bd10988f62135
Mahmuda <24524203082@student.bup.edu.bd>
2026-03-06 16:19:14 +0600

    Create 1829A - Love Story.cpp

---
+++ 1829A - Love Story.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ 
+    int t;
+    cin >> t;
+    const string target = "codeforces";
+ 
+    while (t--) {
+        string s;
+        cin >> s;
+        int diff = 0;
+        for (int i = 0; i < 10; i++) {
+            if (s[i] != target[i])
+                diff++;
+        }
+        cout << diff << "\n";
+    }
+ 
+    return 0;
+}
