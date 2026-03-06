d25371c08976463c431c287fd1e154c00009f750
Mahmuda <24524203082@student.bup.edu.bd>
2026-03-06 15:56:53 +0600

    Create 1772A - A+B?.cpp

---
+++ 1772A - A+B?.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+    for(int i=0; i<t; i++)
+    {
+        string s;
+        cin >> s;
+        int a = s[0] - '0';
+        int b = s[2] - '0';
+ 
+        cout << a + b << endl;
+    }
+ 
+    return 0;
+}
