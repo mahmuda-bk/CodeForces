dcbe23de386e1ed6e27d9a200895fd32452e1417
Mahmuda <24524203082@student.bup.edu.bd>
2026-03-08 19:24:33 +0600

    Create 1845A - Forbidden Integer.cpp

---
+++ 1845A - Forbidden Integer.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while(t--) {
+        int n, k, x;
+        cin >> n >> k >> x;
+ 
+        if (x != 1) {
+            cout << "YES\n";
+            cout << n << endl;
+            for (int i = 0; i < n; i++) cout << 1 << " ";
+            cout << endl;
+            continue;
+        }
+ 
+        if (k == 1) {
+            
+            cout << "NO\n";
+            continue;
+        }
+ 
+        if (n % 2 == 0) {
+            
+            cout << "YES\n";
+            cout << n/2 << endl;
+            for (int i = 0; i < n/2; i++) cout << 2 << " ";
+            cout << endl;
+        } 
+        else {
+            if (k >= 3) {
+                
+                cout << "YES\n";
+                cout << 1 + (n-3)/2 << endl;
+                cout << 3 << " ";
+                for (int i = 0; i < (n-3)/2; i++) cout << 2 << " ";
+                cout << endl;
+            } 
+            else {
+                
+                cout << "NO" << endl;
+            }
+        }
+    }
+    return 0;
+}
