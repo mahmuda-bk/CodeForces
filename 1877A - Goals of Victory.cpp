8a0b7492487dd31362bf03dfb0ded9f586a1f635
Mahmuda <24524203082@student.bup.edu.bd>
2026-03-08 19:20:45 +0600

    Create 1877A - Goals of Victory.cpp

---
+++ 1877A - Goals of Victory.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        int n;
+        cin >> n;
+ 
+        int a[n-1];
+ 
+        for(int i=0; i<n-1; i++)
+        {
+            cin >> a[i];
+        }
+        int sum = 0;
+        for(int i=0; i<n-1; i++)
+        {
+            sum += a[i];
+        }
+ 
+        cout << -sum << endl;
+    }
+    return 0;
+}
