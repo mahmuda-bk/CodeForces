98ab67825af8e61520d122761e8ce8caaf5c64b1
Mahmuda <24524203082@student.bup.edu.bd>
2026-03-08 19:40:50 +0600

    Create 2179A - Blackslex and Password.cpp

---
+++ 2179A - Blackslex and Password.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        int k, x;
+        cin >> k >> x;
+ 
+        int n = k*x+1;
+ 
+        cout << n << endl;
+    }
+    return 0;
+}
