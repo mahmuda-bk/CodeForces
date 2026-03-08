203b82e2978c34f66957bc9838701bbb157e0a41
Mahmuda <24524203082@student.bup.edu.bd>
2026-03-08 19:36:02 +0600

    Create 136A - Presents.cpp

---
+++ 136A - Presents.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+ 
+using namespace std;
+ 
+int main()
+{
+    int n, p;
+    cin >> n;
+    vector<int> a(n+1);
+    for (int i = 1; i <= n; ++i)
+    {
+        cin >> p;
+        a[p] = i;
+    }
+    cout << a[1];
+    for (int i = 2; i <= n; ++i)
+    {
+        cout << " " << a[i];
+    }
+    cout << endl;
+    return 0;
+}
